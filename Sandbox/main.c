15af780edc52d0d4ad3a89bbdbb30c940464c45d
AbramIV <septrakon@gmail.com>
2025-02-13 17:14:05 +0400

    cycles works

--- Sandbox/main.c
+++ Sandbox/main.c
@@ -12,17 +12,31 @@
 #define BIT_CLEAR(REG, BIT)  (REG &= ~(1 << BIT))
 #define BIT_TOGGLE(REG, BIT) (REG ^= (1 << BIT))
 
+#define KEY			BIT_READ(PINB, 0)		    
+
 #define LED			BIT_READ(PORTB, 5)
 #define LED_ON		BIT_SET(PORTB, 5)
 #define LED_OFF		BIT_CLEAR(PORTB, 5)
 #define LED_TOGGLE	BIT_TOGGLE(PORTB, 5)
 
+#define PWM_OUT		 BIT_READ(PORTD, 6)
+#define PWM_OUT_ON	 BIT_SET(PORTD, 6)
+#define PWM_OUT_OFF	 BIT_CLEAR(PORTD, 6)
+
+#define PWM		BIT_READ(TCCR0B, CS02)
 #define PWM_ON  BIT_SET(TCCR0B, CS02)
 #define PWM_OFF BIT_CLEAR(TCCR0B, CS02)
 
-#define INC_DURATION_MAX 10000
-#define DEC_DURATION_MAX 10000
-#define PWM_STEPS_MAX    255
+#define ADC_START  BIT_SET(ADCSRA, ADSC);
+
+#define PWM_MAX		  255
+#define ADC_MAX		  1024
+#define AVG_MAX		  32
+#define DURATION_MAX  10000
+
+#define OFF  0
+#define ON   1
+#define INIT 2
 
 #include <avr/io.h>
 #include <stdio.h>
@@ -31,77 +45,118 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-typedef struct OperationalTimer
+struct sTimer
 {
-	uint16_t ms, ms2, ms5, ms10, ms100, ms200, ms500, s;
-	bool isr;
-} OperationalTimer;
+	uint16_t ms, ms10, ms200, s;
+	bool ovf;
+} timer = {0, 0, 0, 0, false};
 
 struct sVariator
 {
-	 uint16_t duration;
-	 uint16_t count;
-	 uint16_t step;
-	 bool active;
-	 bool isInc;
-} variator = {0, 0, 0, false};
+	uint16_t power;
+	uint16_t duration;
+	uint16_t count;
+	uint16_t step;
+	uint16_t delay;
+	bool active;
+	bool direction;
+} variator = {0, 0, 0, 0, 0, false, false};
 
-OperationalTimer mainTimer = {0, 0, 0, 0, 0, 0, 0, 0, false};
+struct sConverter
+{
+	uint16_t value;
+	bool ready;
+} converter = {0, false};
 
-//sVariator increment = {0, 0, 0, false};
-//sVariator decrement = {0, 0, 0, false};
+bool keyPushed = false;
 
 void GPIO_Init(void);
 void timer0_Init(void);
 void timer1_Init(void);
+void ADC_Init(void);
+
+float durationAvg(uint16_t value, bool reset);
+float powerAvg(uint16_t value, bool reset);
+void keyControl(void);
 
 int main(void)
 {	
 	GPIO_Init();
-	
 	timer0_Init();
 	timer1_Init();
+	ADC_Init();
 	
 	sei();
 	
-	variator.duration = 8000;
-	variator.step = variator.duration/OCR0A;
+	PWM_OFF;
 	
-    while (1) 
+    while (1)
     {
-		if (mainTimer.ms200)
+		if (!variator.delay && !variator.active && !KEY) keyPushed = true;
+		
+		if (keyPushed)
 		{
-			LED_TOGGLE;
-			mainTimer.ms200 = 0;
+			variator.delay = AVG_MAX;
+			keyPushed = false;
 		}
 		
-		if (mainTimer.s)
+		if (variator.active && (variator.count >= variator.step))
 		{
+			if (variator.direction) OCR0A++;
+			else OCR0A--;
 			
-			mainTimer.s = 0;
+			if (OCR0A >= variator.power || OCR0A == 0)
+			{
+				LED_OFF;
+				if (!variator.direction) PWM_OFF;
+				variator.active = false;
+			}	
+			
+			variator.count = 0;
+		}
+		
+		if (converter.ready && !variator.active)
+		{
+			if (ADMUX == 0x40)
+			{
+				variator.duration = (durationAvg(converter.value, false)/ADC_MAX)*DURATION_MAX;
+				ADMUX++;
+			}
+			else
+			{
+				variator.power = (powerAvg(converter.value, false)/ADC_MAX)*PWM_MAX;
+				ADMUX--;
+			}
+			
+			converter.ready = false;
+		}
+		
+		if (timer.ms10)
+		{
+			if (!variator.active) ADC_START;
+			
+			timer.ms10 = 0;
 		}
     }
 }
 
 void GPIO_Init(void)
 {
-	DDRB = 0b00111111;
-	PORTB = 0b00000000;
+	DDRB = 0b00111110;
+	PORTB = 0b00000001;
 	
 	DDRD = 0xFF;
 	PORTD = 0x00;
 	
 	DDRC = 0x00;
-	PORTC = 0xFF;
+	PORTC = 0x00;
 }
 
 void timer0_Init(void)
 {
 	TCCR0A = (1 << COM0A1)|(1 << WGM01)|(1 << WGM00);
 	TCCR0B = (1 << CS02)|(0 << CS01)|(0 << CS00); // divider 256, 1 tick = 16 us
-	OCR0A = 255;
-	
-	TCCR0B = 0x00;
+	OCR0A = 0;
 }
 
 void timer1_Init(void)
@@ -111,33 +166,86 @@ void timer1_Init(void)
 	TCNT1 = 63536; // interrupt in 2000 ticks / 1 tick -> 0.5 us
 }
 
+void ADC_Init(void)
+{
+	ADCSRA = 0x8F;
+	ADMUX = 0x40;
+}
+
+float durationAvg(uint16_t value, bool reset)
+{
+	static float values[AVG_MAX];
+	static unsigned short index = 0;
+	static float result;
+	
+	if (reset)
+	{
+		for (int i=0; i < AVG_MAX; i++) values[i] = 0;
+		result = 0;
+		index = 0;
+		return 0;
+	}
+	
+	result += value - values[index];
+	values[index] = value;
+	index = (index + 1) % AVG_MAX;
+	
+	return result/AVG_MAX;
+}
+
+float powerAvg(uint16_t value, bool reset)
+{
+	static float values[AVG_MAX];
+	static unsigned short index = 0;
+	static float result;
+	
+	if (reset)
+	{
+		for (int i=0; i < AVG_MAX; i++) values[i] = 0;
+		result = 0;
+		index = 0;
+		return 0;
+	}
+	
+	result += value - values[index];
+	values[index] = value;
+	index = (index + 1) % AVG_MAX;
+	
+	return result/AVG_MAX;
+}
+
 ISR(TIMER1_OVF_vect)
 {
-	mainTimer.ms++;
+	timer.ms++;
+	
+	if (variator.delay) 
+	{
+		variator.delay--;
+		
+		if (!variator.delay) 
+		{
+			LED_ON;
+			variator.active = true;
+			variator.direction = PWM ? false : true;
+			variator.step = variator.duration/variator.power;
+			if (variator.direction) PWM_ON;
+		}
+	}
 	
 	if (variator.active) variator.count++;
 	
-	if (variator.count == variator.step)
+	if (timer.ms % 10 == 0) timer.ms10++;
+	if (timer.ms % 1000 == 0) 
 	{
-		if (OCR0A == 255 || !OCR0A) 
-		{
-			variator.active = false;
-			variator.count = 0;
-		}
-		else
-		{
-			if (variator.isInc) OCR0A++;
-			else OCR0A--;
-		}
-	}
-	
-	if (mainTimer.ms % 200 == 0) mainTimer.ms200++;
-	if (mainTimer.ms % 1000 == 0) 
-	{
-		mainTimer.s++;
-		mainTimer.ms = 0;
+		timer.s++;
+		timer.ms = 0;
 	}
 
-	mainTimer.isr = true;
 	TCNT1 = 63536;
+}
+
+ISR(ADC_vect)
+{
+	converter.value = ADCW;
+	converter.ready = true;
 }
